e06b6221b1ec3f1aed8fb2d0db56db2fe2a9c5dd
jamiebeas <128951516+jamiebeas@users.noreply.github.com>
2023-04-08 09:02:47 +0800

    Create rockPaperScissors.js
    Functioning JavaScript code to play Rock, Paper, Scissors.

---
+++ rockPaperScissors.js
@@ -0,0 +1,68 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors" ||
+    userInput === "sharknado"
+  ) {
+    return userInput;
+  } else {
+    console.log("Error!");
+  }
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "Unlucky. The game was a tie.";
+  }
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "You lost to a computer!";
+    } else {
+      return "Congratulations. You beat the computer!";
+    }
+  }
+
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "You lost to a computer!";
+    } else {
+      return "Congratulations. You beat the computer!";
+    }
+  }
+
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "You lost to a computer!";
+    } else {
+      return "Congratulations. You beat the computer!";
+    }
+  }
+
+  if (userChoice === "sharknado") {
+    return "You win, cheater!"
+  }
+};
+
+const playGame = () => {
+  const userChoice = getUserChoice("sharknado");
+  const computerChoice = getComputerChoice();
+  console.log("You threw: " + userChoice);
+  console.log("The computer threw: " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
